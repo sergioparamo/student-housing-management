caf3105fda9aecad38dd3af33b3aea8c5a50531b
sergioparamo <sergio.paramo1997@gmail.com>
2021-05-03 17:08:22 +0200

    first commit

---
+++ src/main/java/cat/itb/studenthousingweb/StudentHousingWebApplication.java
@@ -0,0 +1,13 @@
+package cat.itb.studenthousingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHousingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHousingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itb/studenthousingweb/StudentHousingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itb.studenthousingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHousingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
